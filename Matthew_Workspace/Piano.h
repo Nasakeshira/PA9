162b0b32a6ce8589bef9a217ac394f141c1fb610
Nasakeshira <Nasakeshira@users.noreply.github.com>
2020-04-24 10:49:16 -0700

    Create Piano.h
    Created class prototype Piano.h

---
+++ Matthew_Workspace/Piano.h
@@ -0,0 +1,29 @@
+#ifndef PIANO_H
+#define PIANO_H
+// May not need <iostream>
+#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+class Piano
+{
+private:
+
+public:
+// Tentative structure for programs
+void playSound();
+
+void displayKeys();
+
+// Maybe include a thing where people can play a piano together? Using <SFML/Network.hpp>
+void displayMenu();
+
+void downShiftOctave();
+
+void upShiftOctave();
+
+};
+
+#endif
