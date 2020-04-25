39d4fd2b922e2669c4f34e475b5896f715657f1e
MikeSallee10 <62958937+MikeSallee10@users.noreply.github.com>
2020-04-25 13:04:59 -0700

    Update Piano.cpp

--- Matthew_Workspace/Piano.cpp
+++ Matthew_Workspace/Piano.cpp
@@ -8,6 +8,38 @@ SoundBuffer Piano::loadSound()
 	SoundBuffer buffer;
 	
 	buffer.loadFromSamples(&samples[0], samples.size(), 2, 44100);
+	
+	while(window.isOpen())
+	{
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			sound.play()
+		}
+		if(sf::KeyBoard::isKeyPressed(sf::Keyboard::B))
+		{
+			soudn.play()
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+		{
+			sound.play()
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			sound.play()
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+		{
+			sound.play()
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+		{
+			sound.play()
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+		{
+			sound.play()
+		}
+	}		
 
 	return buffer;
 }
