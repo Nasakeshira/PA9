53e6fad895a29c507ed173e4b7a1e8cc95280696
JohnL20 <63315790+JohnL20@users.noreply.github.com>
2020-05-01 16:33:02 -0700

    Update Piano.cpp

--- Matthew_Workspace/Piano.cpp
+++ Matthew_Workspace/Piano.cpp
@@ -15,25 +15,25 @@ Piano::~Piano()
 void Piano::loadSound()
 {
 	SoundBuffer buffer[7];
-	
+
 	// buffer.loadFromSamples(&samples[0], samples.size(), 2, 44100);
 
 	// loadFromfile need to sample all the correct audio files
 
 	//A
-	buffer[0].loadFromFile("sound.wav");
+	buffer[0].loadFromFile("1g - white 1.wav");
 	//B
-	buffer[1].loadFromFile("sound.wav");
+	buffer[1].loadFromFile("3b - white 2.wav");
 	//C
-	buffer[2].loadFromFile("sound.wav");
+	buffer[2].loadFromFile("5c - white 3.wav");
 	//D
-	buffer[3].loadFromFile("sound.wav");
+	buffer[3].loadFromFile("6c# - white 4.wav");
 	//E
-	buffer[4].loadFromFile("sound.wav");
+	buffer[4].loadFromFile("8eb - white 5.wav");
 	//F
-	buffer[5].loadFromFile("sound.wav");
+	buffer[5].loadFromFile("10f - white 6.wav");
 	//G
-	buffer[6].loadFromFile("sound.wav");
+	buffer[6].loadFromFile("12 - white 7.wav");
 
 	for (int index = 0; index < 7; index++)
 	{
@@ -43,7 +43,6 @@ void Piano::loadSound()
 
 /* void playSound()
 {
-
 	while(m_window.isOpen())
 	{
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
