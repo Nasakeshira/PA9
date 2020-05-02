6319ba42b40f08055e75785d3c03e7ab89d6c23d
JohnL20 <63315790+JohnL20@users.noreply.github.com>
2020-05-01 18:31:57 -0700

    Update Piano.h

--- Matthew_Workspace/Piano.h
+++ Matthew_Workspace/Piano.h
@@ -10,18 +10,18 @@ class Piano
 private:
 
 public:
-// Tentative structure for programs
+	// Tentative structure for programs
 	Piano();
 
 	~Piano();
 
 	virtual void loadSound();
 
-// Maybe include a thing where people can play a piano together? Using <SFML/Network.hpp>
+	// Maybe include a thing where people can play a piano together? Using <SFML/Network.hpp>
 
-// void downShiftOctave(); if isKeyPressed right shift
+	// void downShiftOctave(); if isKeyPressed right shift
 
-// void upShiftOctave(); if isKeyPressed left shift
+	// void upShiftOctave(); if isKeyPressed left shift
 
 };
 
